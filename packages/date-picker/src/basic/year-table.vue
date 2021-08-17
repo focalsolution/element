ef18f8fd542ad5f3a29cec5f5d5f71dc801dfe87
Rachapong Pornwiriyangkura <rachapong@focalsolution.com>
2021-08-17 11:41:22 +0700

    fix: Buddhist year does not show in Year panel

--- packages/date-picker/src/basic/year-table.vue
+++ packages/date-picker/src/basic/year-table.vue
@@ -3,38 +3,38 @@
     <tbody>
     <tr>
       <td class="available" :class="getCellStyle(startYear + 0)">
-        <a class="cell">{{ startYear }}</a>
+        <a class="cell">{{ startYear + 543 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 1)">
-        <a class="cell">{{ startYear + 1 }}</a>
+        <a class="cell">{{ startYear + 543 + 1 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 2)">
-        <a class="cell">{{ startYear + 2 }}</a>
+        <a class="cell">{{ startYear + 543 + 2 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 3)">
-        <a class="cell">{{ startYear + 3 }}</a>
+        <a class="cell">{{ startYear + 543 + 3 }}</a>
       </td>
     </tr>
     <tr>
       <td class="available" :class="getCellStyle(startYear + 4)">
-        <a class="cell">{{ startYear + 4 }}</a>
+        <a class="cell">{{ startYear + 543 + 4 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 5)">
-        <a class="cell">{{ startYear + 5 }}</a>
+        <a class="cell">{{ startYear + 543 + 5 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 6)">
-        <a class="cell">{{ startYear + 6 }}</a>
+        <a class="cell">{{ startYear + 543 + 6 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 7)">
-        <a class="cell">{{ startYear + 7 }}</a>
+        <a class="cell">{{ startYear + 543 + 7 }}</a>
       </td>
     </tr>
     <tr>
       <td class="available" :class="getCellStyle(startYear + 8)">
-        <a class="cell">{{ startYear + 8 }}</a>
+        <a class="cell">{{ startYear + 543 + 8 }}</a>
       </td>
       <td class="available" :class="getCellStyle(startYear + 9)">
-        <a class="cell">{{ startYear + 9 }}</a>
+        <a class="cell">{{ startYear + 543 + 9 }}</a>
       </td>
       <td></td>
       <td></td>
